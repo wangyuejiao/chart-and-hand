06c52d0bb4d84cc2c6f42b0421eede784ed524cb
王月娇 <1954896440@qq.com>
2020-03-25 15:09:25 +0800

    add 1

--- Encharts/index.js
+++ Encharts/index.js
@@ -39,4 +39,30 @@ $(function() {
     };
     //使用刚刚指定的配置项和数据项显示图表
     myChart.setOption(option);
+
+
+
+    var data =[
+        ['Java',1,'降','-0.01%'],
+        ['C',2,'升','+2.44%'],
+        ['Python',3,'升','+1.41%'],
+        ['C++',4,'降','-2.58%'],
+        ['C#',5,'升','+2.07%'],
+        ['Visual Basic.NET',6,'降','-1.17%'],
+        ['JavaScript',7,'降','-0.85%']
+    ];
+      
+      var container = document.getElementById('example');
+      var hot = new Handsontable(container, {
+        data: data,
+        className: "htCenter htMiddle", 
+        colHeaders: true,
+        colHeaders: ['语言名称','排名','升或降','变化幅度'],
+        rowHeights: 40,
+        colWidths: 200,
+        contextMenu: true,          
+        manualRowResize : true,      
+        manualColumnResize : true,
+            
+      });
   });
